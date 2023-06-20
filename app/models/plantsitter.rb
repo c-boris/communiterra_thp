0333a84b70ac1ddf7950f1180f023b5500c47e96
cboris <b.crochet@gmail.com>
2023-06-20 17:40:39 +0200

    fix

--- app/models/plantsitter.rb
+++ app/models/plantsitter.rb
@@ -1,4 +1,4 @@
-class PlantSitter < User
-  has_many :kept_plants_as_sitter, foreign_key: :sitter_id, class_name: 'KeptPlant'
-  has_many :plant_sittings_as_sitter, through: :kept_plants_as_sitter, source: :plant_sitting
+class Plantsitter < User
+  has_many :plant_sittings_as_asker, foreign_key: :asker_id, class_name: 'PlantSitting'
+  has_many :kept_plants_as_asker, through: :plant_sittings_as_asker, source: :kept_plant
 end
